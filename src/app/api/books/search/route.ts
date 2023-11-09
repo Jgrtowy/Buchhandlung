b7412f2b2e0d4b5afb0befd015b7ef52453d8517
Jgrtowy <panjogurcik@outlook.com>
2023-11-09 12:52:15 +0100

    Add searchbar, add and delete book buttons

--- src/app/api/books/search/route.ts
+++ src/app/api/books/search/route.ts
@@ -1,15 +1,43 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type ksiazki } from '@prisma/client';
-import type { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 import { db } from '~/server/db';
 
 export async function POST(req: NextRequest) {
-    console.log(req.body);
+    const data: any = await req.json();
+    if (data.body.search === '') {
+        try {
+            const books: ksiazki[] = await db.ksiazki.findMany();
+            return Response.json(books);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     try {
-        const books: ksiazki[] = await db.ksiazki.findMany();
+        const books: ksiazki[] = await db.ksiazki.findMany({
+            where: {
+                OR: [
+                    {
+                        autor: {
+                            startsWith: data.body.search,
+                        },
+                    },
+                    {
+                        tytul: {
+                            startsWith: data.body.search,
+                        },
+                    },
+                ],
+            },
+        });
         return Response.json(books);
     } catch (error) {
         console.log(error);
-        return Response.json({ error });
     }
 }
--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/non-nullable-type-assertion-style */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/prefer-optional-chain */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @next/next/no-async-client-component */
@@ -7,20 +10,13 @@
 import type { ksiazki } from '@prisma/client';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { modal } from '~/utils/swal';
 
 export default function Books() {
     const [data, setData] = useState<ksiazki[]>([]);
 
-    useEffect(() => {
-        const dataFetch = async () =>
-            await fetch('http://localhost:3000/api/books/get')
-                .then((res) => res.json())
-                .then((res) => setData(res));
-        dataFetch().catch((error) => console.error(error));
-    }, []);
-
     const [search, setSearch] = useState('');
-
+    const [dataChanged, setDataChanged] = useState<boolean>(false);
     useEffect(() => {
         const dataFetch = async () =>
             await axios
@@ -31,18 +27,91 @@ export default function Books() {
                 })
                 .then((res) => setData(res.data));
         dataFetch().catch((error) => console.error(error));
-    }, [search]);
+    }, [search, dataChanged]);
 
+    const addPopup = async () => {
+        const form = await modal.fire({
+            title: 'Dodaj książkę',
+            html: `
+            <form class="flex flex-col gap-3">
+                <input class="px-3 py-2 bg-transparent outline-none border-2 border-[#57bd8a]" placeholder="Tytuł" type="text" data-form-type="other" name="title"/>
+                <input class="px-3 py-2 bg-transparent outline-none border-2 border-[#57bd8a]" placeholder="Autor" type="text" data-form-type="other" name="author"/>
+                <input class="px-3 py-2 bg-transparent outline-none border-2 border-[#57bd8a]" placeholder="Gatunek" type="text" data-form-type="other" name="genre"/>
+                <input class="px-3 py-2 bg-transparent outline-none border-2 border-[#57bd8a]" placeholder="Data wydania" type="date" data-form-type="other" name="date"/>
+            </form>
+            `,
+            confirmButtonText: 'Dodaj',
+            preConfirm: () => {
+                const form = document.querySelector('form') as HTMLFormElement;
+                const formData = new FormData(form);
+                const data = Object.fromEntries(formData.entries());
+                const values = {
+                    title: data.title,
+                    author: data.author,
+                    genre: data.genre,
+                    releaseDate: data.date ?? new Date(),
+                };
+                return values;
+            },
+        });
+
+        if (!form.isConfirmed) return;
+
+        const { title, author, genre, releaseDate } = form.value;
+
+        try {
+            await axios
+                .post(`http://localhost:3000/api/books/add`, {
+                    body: {
+                        title: title,
+                        author: author,
+                        genre: genre,
+                        releaseDate: releaseDate,
+                    },
+                })
+                .then((res) => console.log(res.data.message === 'success'));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const deleteBook = async (id: number) => {
+        await axios.post('http://localhost:3000/api/books/delete', {
+            body: {
+                id: id,
+            },
+        });
+    };
     return (
-        <div className="flex flex-col text-white w-100 h-screen">
-            <input type="text" onInput={(e) => setSearch(e.currentTarget.value)} value={search} className="w-25" />
-            {data.map((book) => (
-                <div key={book.id_k} className="flex justify-center w-100">
-                    <h1>{book.tytul}</h1>
-                    <h2>{book.autor}</h2>
-                    <h3>{book.gatunek}</h3>
+        <div className="flex flex-col items-center text-white w-100 h-screen gap-5">
+            <div className="flex gap-5 mt-5">
+                <input type="text" onInput={(e) => setSearch(e.currentTarget.value)} value={search} className="w-25 text-white px-3 py-1 h-12 bg-transparent outline-none border-2 border-[#57bd8a] rounded-lg text-2xl" />
+                <button onClick={addPopup} className="bg-transparent border-2  border-[#57bd8a] outline-none rounded-lg text-2xl px-3 py-1 h-12">
+                    Dodaj
+                </button>
+            </div>
+
+            {data.length !== 0 && (
+                <div className="w-screen flex flex-col items-center">
+                    <div className="flex justify-around w-screen">
+                        <div className="text-3xl w-72">Tytuł</div>
+                        <div className="text-3xl w-72">Autor</div>
+                        <div className="text-3xl w-72">Gatunek</div>
+                        <div className="text-3xl w-72">Data wydania</div>
+                    </div>
+                    {data.length !== 0 &&
+                        data.map((book) => (
+                            <div key={book.id_k} className="flex justify-around w-screen">
+                                <button onClick={() => deleteBook(book.id_k)}>Usuń</button>
+                                <div className="text-lg w-72 ">{book.tytul}</div>
+                                <div className="text-lg w-72 ">{book.autor}</div>
+                                <div className="text-lg w-72 ">{book.gatunek}</div>
+                                <div className="text-lg w-72 ">{book.data_wydania?.toLocaleString()}</div>
+                            </div>
+                        ))}
                 </div>
-            ))}
+            )}
+            {data.length === 0 && <h1 className="text-6xl">Nie znaleziono książek</h1>}
         </div>
     );
 }
